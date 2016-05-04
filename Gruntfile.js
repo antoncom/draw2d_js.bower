d3bf975715e8b1fe20a472179c19c869a304d8de
Andreas Herz <a.herz@freegroup.de>
2016-05-04 06:46:33 +0200

    push th correct js files

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function (grunt) {
                 updateConfigs: [],
                 commit: true,
                 commitMessage: 'Release v%VERSION%',
-                commitFiles: ['package.json'],
+                commitFiles: ['package.json', "dist/*.js"],
                 createTag: true,
                 tagName: 'v%VERSION%',
                 tagMessage: 'Version %VERSION%',
