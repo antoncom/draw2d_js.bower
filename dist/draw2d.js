f990a2fd0c9e5fa8a1a14a8ecf52a25e5bac20b9
Andreas Herz <a.herz@freegroup.de>
2016-08-06 00:04:37 +0200

    Release v6.1.52

--- dist/draw2d.js
+++ dist/draw2d.js
@@ -19811,7 +19811,7 @@ draw2d.policy.port.IntrusivePortsFeedbackPolicy = draw2d.policy.port.PortFeedbac
  *   Library is under GPL License (GPL)
  *   Copyright (c) 2012 Andreas Herz
  ****************************************/draw2d.Configuration = {
-    version : "6.1.51",
+    version : "6.1.52",
     i18n : {
         command : {
             move : "Move Shape",
